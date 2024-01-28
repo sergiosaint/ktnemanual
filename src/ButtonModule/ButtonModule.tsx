dedb75535eea1010d59cb1b804846aed7bed17b6
sergiosaint <sergio.n.lopes@gmail.com>
2024-01-28 16:03:43 +0000

    fix

--- src/ButtonModule/ButtonModule.tsx
+++ src/ButtonModule/ButtonModule.tsx
@@ -1,21 +1,19 @@
-﻿import './ButtonModule.css';
-import ButtonTable from './img/button.png';
-
+﻿
 function ButtonModule() {
-  return (
-      <div>
-          <h3>The Button</h3>
-          <img src={ButtonTable} alt="" width="100%" />
+    return (
+        <div>
+            <h3>The Button</h3>
+            <img src={ButtonTable} alt="" width="100%" />
 
-          Alternative:<br/>
+          Alternative:<br />
           PRESS and RELEASE:<br />
               ≥2 batteries & “Detonate” and NOT (blue & “Abort”)<br />
               ≥3 batteries & label FRK and NOT (blue & “Abort”) and NOT (white & label CAR )<br />
 red & “Hold”<br />
 
 Otherwise HOLD<br />
-      </div>
-  );
+        </div>
+    );
 }
 
 export default ButtonModule;
