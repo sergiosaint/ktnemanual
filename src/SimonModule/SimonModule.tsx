c0e5d1e689e03e150d5e4d6d20b7be5369a21a4c
sergiosaint <sergio.n.lopes@gmail.com>
2024-01-28 16:00:29 +0000

    identation

--- src/SimonModule/SimonModule.tsx
+++ src/SimonModule/SimonModule.tsx
@@ -2,12 +2,12 @@ import './SimonModule.css';
 import simonImg from './img/simon.png';
 
 function SimonModule() {
-  return (
-      <div>
-          <h3>Simon Says</h3>
-          <img src={simonImg} width="100%" alt="" />
-      </div>
-  );
+    return (
+        <div>
+            <h3>Simon Says</h3>
+            <img src={simonImg} width="100%" alt="" />
+        </div>
+    );
 }
 
 export default SimonModule;
